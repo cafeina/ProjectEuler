3c19bec9a481903ed06ea106d06977c9ef9c1889
Gleison Batista <gleisonbs@gmail.com>
2015-04-13 02:26:26 -0300

    Solution to problem 2

--- 1/1.cpp
+++ 1/1.cpp
@@ -9,13 +9,12 @@ using namespace std;
 int main()
 {
 	vector<int> multiples(1000);
-	int sum;
 	for(int n = 0; n <1000; ++n)
 		if((n % 3 == 0) or (n % 5 == 0))
 			multiples.push_back(n);
 
-	sum = accumulate(multiples.begin(), multiples.end(), 0);
+	int sum = accumulate(multiples.begin(), multiples.end(), 0);
 	cout << "Answer: " << sum << endl;
 
-
+	return 0;
 }
---
+++ 2/2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+	int previous = 1;
+	int current = 2;
+	int temp;
+
+	vector<int> primes;
+	while(current < 4000000)
+	{
+		if(current % 2 == 0)
+			primes.push_back(current);
+
+		temp = current + previous;
+		previous = current;
+		current = temp;		
+	}
+
+	int sum = accumulate(primes.begin(), primes.end(), 0);
+
+	cout << "Answer: " << sum << endl;
+
+	return 0;
+}
